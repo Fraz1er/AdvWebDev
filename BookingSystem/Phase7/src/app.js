87b28a31f3d3a7ab3fb48c73e7904f844a8d7633
Igor Smirnov <igorvor6@gmail.com>
2026-03-17 16:32:58 +0200

    a

--- BookingSystem/Phase7/src/app.js
+++ BookingSystem/Phase7/src/app.js
@@ -5,6 +5,7 @@ import reservationsRouter from "./routes/reservations.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import { requireAuth } from "./middleware/auth.middleware.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
